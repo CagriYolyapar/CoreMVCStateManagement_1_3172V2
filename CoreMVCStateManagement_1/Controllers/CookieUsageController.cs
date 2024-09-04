ce3b31af458fbc921e0b43845f819d484306f4a2
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-09-04 14:37:30 +0300

    Add project files.

---
+++ CoreMVCStateManagement_1/Controllers/CookieUsageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCStateManagement_1.Controllers
+{
+    public class CookieUsageController : Controller
+    {
+
+        void SetCookie()
+        {
+            HttpContext.Response.Cookies.Append("Cagri", "CyberPunk Fan", new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(1),
+                HttpOnly = true, //document.cookie ile cookielere ulasılamaz...
+                SameSite = SameSiteMode.Strict
+
+            });
+        }
+
+
+        string GetCookie()
+        {
+            HttpContext.Request.Cookies.TryGetValue("Cagri", out string cookie);
+            return cookie;
+
+        }
+
+
+        public IActionResult CookieIndex()
+        {
+            SetCookie();
+
+            ViewBag.Message = GetCookie();
+            
+            return View();
+        }
+    }
+}
